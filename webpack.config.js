d5c3456ea01b2abe6fe9b8ee465a6ff18dfd4604
keni <yangkeni666@gmail.com>
2022-12-23 16:34:23 +0800

    feat: update blog logo

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,25 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 /**@type {import('webpack').Configuration} */
 
 module.exports = {
   optimization: {
-    minimizer: [new TerserPlugin({
-      terserOptions: {
-        compress: {
-          unused: true,
-          drop_console: true,
-          drop_debugger: true,
-          dead_code: true
-        }
-      }
-    })]
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            unused: true,
+            drop_console: true,
+            drop_debugger: true,
+            dead_code: true,
+          },
+        },
+      }),
+    ],
   },
   entry: path.resolve(__dirname, 'src/index.js'),
   output: {
@@ -45,6 +48,19 @@ module.exports = {
         test: /\.less$/,
         exclude: /node_modules/,
         use: ['style-loader', 'css-loader', 'less-loader'],
+      },
+      {
+        // 图片格式处理
+        test: /\.(jpg|png|gif|svg)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            // 定义打包后文件的名称；
+            // [name]:原文件名，[hash]:hash字符串（如果不定义名称，默认就以hash命名，[ext]:原文件的后缀名）
+            name: '[name]_[hash].[ext]',
+            outputPath: 'images/', //  定义图片输出的文件夹名（在output.path目录下）
+          },
+        },
       },
     ],
   },
